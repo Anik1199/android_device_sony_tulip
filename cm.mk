a47e35a0519c69b7eaedf4db7ae91b466a513876
Anik <anik9280@gmail.com>
2015-12-04 13:23:23 +0600

    tulip: add CM makefile

---
+++ cm.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit device specific parts
+$(call inherit-product, device/sony/tulip/aosp_d2303.mk)
+$(call inherit-product, device/sony/kanuti-common/device.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Override Product Name
+PRODUCT_NAME := cm_tulip
+PRODUCT_MODEL := Xperia M4 Aqua
